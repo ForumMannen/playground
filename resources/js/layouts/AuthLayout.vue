1cc02d27c74c424c7e7aba410f750b0505d19888
ForumMannen <lundell.linus@gmail.com>
2025-05-04 23:47:48 +0200

    deleted alot and fixed app.ts

--- resources/js/layouts/AuthLayout.vue
+++ resources/js/layouts/AuthLayout.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import AuthLayout from '@/layouts/auth/AuthSimpleLayout.vue';
-
 defineProps<{
     title?: string;
     description?: string;
--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -1,41 +1,21 @@
+import '../css/app.css';
+
 import { createInertiaApp } from '@inertiajs/vue3';
-import createServer from '@inertiajs/vue3/server';
-import { renderToString } from '@vue/server-renderer';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createSSRApp, h } from 'vue';
-import { route as ziggyRoute } from 'ziggy-js';
+import { createApp, h, type DefineComponent } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Sparcnet';
 
-createServer((page) =>
-    createInertiaApp({
-        page,
-        render: renderToString,
-        title: (title) => `${title} - ${appName}`,
-        resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
-        setup({ App, props, plugin }) {
-            const app = createSSRApp({ render: () => h(App, props) });
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+createInertiaApp({
+    title: (title) => (title ? `${title} - ${appName}` : appName),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        const vueApp = createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue);
 
-            // Configure Ziggy for SSR...
-            const ziggyConfig = {
-                ...page.props.ziggy,
-                location: new URL(page.props.ziggy.location),
-            };
-
-            // Create route function...
-            const route = (name: string, params?: any, absolute?: boolean) => ziggyRoute(name, params, absolute, ziggyConfig);
-
-            // Make route function available globally...
-            app.config.globalProperties.route = route;
-
-            // Make route function available globally for SSR...
-            if (typeof window === 'undefined') {
-                global.route = route;
-            }
-
-            app.use(plugin);
-
-            return app;
-        },
-    }),
-);
+        vueApp.mount(el);
+    },
+});
